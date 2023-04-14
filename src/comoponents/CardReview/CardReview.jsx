1135f4bb2ab01885bca7434d2f2e3c9dbec7117c
kevin170703 <correakevinfabian01@gmail.com>
2023-04-13 21:21:12 -0300

    mejora de responsive cards

--- src/comoponents/CardReview/CardReview.jsx
+++ src/comoponents/CardReview/CardReview.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import style from "./CardReview.module.css";
 import avatar from "../../assets/avatar.png";
-export default function CardReview() {
+export default function CardReview({ reviewText, nameUser }) {
   return (
     <div className={style.contentAll}>
       <div className={style.contetInfoUser}>
         <img src={avatar} alt="" />
-        <h6>Kevin Correa</h6>
+        <h6>{nameUser}</h6>
       </div>
-      <p className={style.reviewText}>
-        Muy buen servicio, me ayudo a conseguir mi primer trabajo luego de la
-        revision de mi CV.
-      </p>
+      <p className={style.reviewText}>{reviewText}</p>
     </div>
   );
 }
