06c114de3ed4ca12462598e257cce633eae56bb4
kevin170703 <correakevinfabian01@gmail.com>
2023-04-21 15:12:43 -0300

    Solucionando errores de keys

--- src/comoponents/CardReview/CardReview.jsx
+++ src/comoponents/CardReview/CardReview.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./CardReview.module.css";
 import avatar from "../../assets/avatar.png";
+
 export default function CardReview({ reviewText, nameUser }) {
   return (
     <div className={style.contentAll}>
