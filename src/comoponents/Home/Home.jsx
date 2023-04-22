e7806857f6a9a0c466340635c4cc0c882a6a8410
kevin170703 <correakevinfabian01@gmail.com>
2023-04-22 11:41:21 -0300

    Solucinando bugs de navbar

--- src/comoponents/Home/Home.jsx
+++ src/comoponents/Home/Home.jsx
@@ -4,21 +4,32 @@ import { Link } from "react-scroll";
 import backgroundHome from "../../assets/background-home.png";
 import backgroundHomeMovile from "../../assets/backgroud-home-mobile.png";
 import backgroundHomeMid from "../../assets/background-home-mid.png";
+import backgroundHomeMovileLow from "../../assets/background-movil-low.png";
 
 import backgroundVideo from "../../assets/video-home.mp4";
 
 export default function Home() {
   return (
     <div className={style.contentAll} id="home">
-      {innerWidth > 1200 && (
+      {innerWidth > 1200 ? (
         <img src={backgroundHome} alt="" className={style.background} />
-      )}
-      {innerWidth < 1300 && (
+      ) : null}
+
+      {innerWidth <= 1200 && innerWidth > 700 ? (
         <img src={backgroundHomeMid} alt="" className={style.background} />
-      )}
-      {innerWidth < 700 && (
+      ) : null}
+
+      {innerWidth <= 700 && innerWidth > 400 ? (
         <img src={backgroundHomeMovile} alt="" className={style.background} />
-      )}
+      ) : null}
+
+      {innerWidth <= 400 ? (
+        <img
+          src={backgroundHomeMovileLow}
+          alt=""
+          className={style.background}
+        />
+      ) : null}
 
       <video autoPlay muted loop className={style.backgroundVideo}>
         <source src={backgroundVideo} type="video/mp4" />
