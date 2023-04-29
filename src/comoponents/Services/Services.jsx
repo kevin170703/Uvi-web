cf558ac09a2002c2e7c13eb436d8b628890f5648
kevin170703 <correakevinfabian01@gmail.com>
2023-04-28 21:28:28 -0300

    creando componente y añadiendole funcionalidad respecto al diseño

---
+++ src/comoponents/Services/Services.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import style from "./Services.module.css";
+import { Reorder } from "framer-motion";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import CardService from "../CardService/CardService";
+
+export default function Services() {
+  const [items, setItems] = useState([0, 1, 2, 3]);
+  const [isOpen, setIsOpen] = useState({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+  });
+
+  function changueOpen(number) {
+    setIsOpen({
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+      [number]: !isOpen[number],
+    });
+  }
+
+  return (
+    <div className={style.contentAll}>
+      <div className={style.barra}></div>
+      <h3 className={style.title}>Servicios</h3>
+      <div className={style.card}>
+        <h5 className={style.titleCard}>Caracteristicas</h5>
+        <p className={style.subtitleCard}>Descripcion de seleccion</p>
+        <Reorder.Group
+          axis="y"
+          values={items}
+          onReorder={setItems}
+          className={style.contentCards}
+        >
+          {items.map((item) => (
+            <Reorder.Item key={item} value={item}>
+              <CardService
+                isOpen={isOpen[item]}
+                number={item}
+                functionChangue={changueOpen}
+                character={"caracter 1"}
+              />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+        <span>
+          Donacion mas popular <br />
+          $$$
+        </span>
+        <button className={style.buttonDonate}>Donar</button>
+      </div>
+    </div>
+  );
+}
