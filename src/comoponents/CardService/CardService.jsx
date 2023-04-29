bc78b40531a2fddf2a47c72e0c0b1a14a4344506
kevin170703 <correakevinfabian01@gmail.com>
2023-04-28 21:28:19 -0300

    creando componente y añadiendole funcionalidad respecto al diseño

---
+++ src/comoponents/CardService/CardService.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import style from "./CardService.module.css";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+export default function CardService({
+  character,
+  functionChangue,
+  number,
+  isOpen,
+}) {
+  return (
+    <motion.div className={style.contentAll} data-isOpen={isOpen}>
+      <div className={style.contentPrimary}>
+        <input type="checkbox" />
+        <h6 className={style.title}>{character}</h6>
+        <IoIosArrowDown
+          size="25"
+          className={style.arrow}
+          onClick={() => functionChangue(number)}
+        />
+      </div>
+
+      <p className={style.infoMore}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laborum
+        fugiat facilis, rerum, minima
+      </p>
+    </motion.div>
+  );
+}
